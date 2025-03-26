e8ed81d66fd00da165cb6a9d1700c5826da83234
Syahrul Adiguna Kurniawan <syahrulzzadie@gmail.com>
2025-03-26 09:40:30 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'syahrulzzadie/bot-telegram-php',
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'syahrulzzadie/bot-telegram-php' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
